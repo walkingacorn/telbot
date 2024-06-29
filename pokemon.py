663c295ad1a22bbfcb9984e804542e8a869cc159
Anjali <109271436+walkingacorn@users.noreply.github.com>
2024-06-29 14:57:41 +0530

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -5,9 +5,9 @@ def getPokemonStats(name):
     r = requests.get('https://pokeapi.co/api/v2/pokemon/'+name)
     if r.status_code == 200:
         a = r.json()
-        b = a['stats']
+        c = a['stats']
         output = ''
-        for i in b:
+        for i in c:
            output = output + i['stat']['name']+':'+str(i['base_stat'])+'\n'
         return output
     else:
